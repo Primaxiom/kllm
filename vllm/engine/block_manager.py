ecdf6288495ec60f1ce4116ad046f56a191ff486
Kagamin0707 <1654824865@qq.com>
2026-03-05 10:16:24 +0800

    chore(engine): 添加内存块管理器类注释 - 此外还修复了一个严重问题

--- vllm/engine/block_manager.py
+++ vllm/engine/block_manager.py
@@ -10,6 +10,12 @@ class Block:
     self,
     block_id: int,
   ):
+    '''
+ref_count > 0 时,
+不应当存在 len(token_ids) != BlockManager.block_size 的块,
+更不应当存在 len(token_ids) != BlockManager.block_size 且 hash != -1 的块,
+也不应当存在 len(token_ids) == BlockManager.block_size 且 hash == -1 的块
+'''
     self.block_id   = block_id
     self.hash       = -1
     self.ref_count  = 0
@@ -34,6 +40,10 @@ class BlockManager:
     num_blocks: int, 
     block_size: int
   ):
+    '''
+BlockManager 中涉及的 block_id 均为物理块号
+hash_to_block_id 即为前缀哈希到物理块号的映射
+'''
     self.block_size:        int             = block_size
     self.blocks:            list[Block]     = [Block(i) for i in range(num_blocks)]
     self.hash_to_block_id:  dict[int, int]  = {}
@@ -46,6 +56,15 @@ class BlockManager:
     token_ids:    list[int], 
     prefix_hash:  int = -1 ,
   ) -> int:
+    '''
+compute_hash
+
+记字符串 s 的哈希值为 prefix_hash, 
+  字符串 t 的 token 为 token_ids,
+该函数计算字符串 s + t 的哈希值
+
+这保证每个 Block 的哈希值包含前缀的所有内容 
+'''
     h = xxhash.xxh64()
     if prefix_hash != -1:
       h.update(prefix_hash.to_bytes(8, "little"))
@@ -70,6 +89,29 @@ class BlockManager:
     return seq.num_blocks <= len(self.free_block_ids)
   
   def allocate(self, seq: Sequence):
+    '''
+allocate
+
+该函数为一整个 Sequence 的 token 分配空间, 
+分配前应当保证其页表为空, 
+即上一次分配的空间应当全部已经释放
+
+序列是否有足够空间分配应当由调用者使用 BlockManager.can_allocate 进行检查
+
+i 为逻辑块号, 在 Sequence 中的表现为连续存储, 使用 seq.block_table 转换成物理块号
+
+对于每个完整块 i, 计算块 [0, i] 的 token 的哈希值作为该块的哈希值,
+保证复用块时的 token 前缀一致,
+不完整块的哈希值应当为 -1
+
+若块 i 的哈希值若在 self.hash_to_block_id 中有记录则可无需再次分配, 
+则只需增加引用计数,
+否则分配 self.free_block_ids 队头元素为编号的内存块
+
+若块 i 的哈希值有效, 
+则更新 self.hash_to_block_id,
+最后在 seq.block_table 记录物理块号
+'''
     assert seq.block_table == [], f"序列 {seq.seq_id} 页表非空"
     hash        = -1
     cache_miss  = False
@@ -96,6 +138,7 @@ class BlockManager:
 虽然 physical_block_id not in self.used_block_ids 说明该块被 _deallocate_block, 
 但是 not cache_miss 说明 physical_block_id != -1 且 token_ids == self.blocks[physical_block_id],
 也即 self.hash_to_block_id 中存在的映射仍然有效, 可以复用
+此处是唯一会导致 deque 随机访问的情况, 不过一般情况下发生概率貌似很小 
 '''
           block = self._allocate_block(physical_block_id)
 
@@ -116,13 +159,24 @@ class BlockManager:
     seq.block_table.clear()
 
   def can_append(self, seq: Sequence) -> bool:
-    # 若余 1, 则需要新块, 否则当前序列的最后一个 token 可以加到最后一个块
+    # 若 len(seq) % self.block_size 余 1, 则需要新块, 否则当前序列的最后一个 token 可以加到最后一个块
     return len(self.free_block_ids) >= (len(seq) % self.block_size == 1)
 
   def may_append(self, seq: Sequence):
+    '''
+may_append
+
+decode 阶段使用的空间分配函数, 有新 token 时调用,
+简化版的 allocate 函数, 讨论 3 种情况:
+1. 该 token 不属于 last_block, 需要分配新块
+2. 该 token 是 last_block 的最后一个 token, 更新 last_block 和 hash_to_block_id
+3. 该 token 不是任何一个前述情况, 无需任何操作
+
+每个块的分配和内容更新是分离的, 只有该块完整时才更新
+'''
     block_table             = seq.block_table
-    last_physical_block_id  = seq.block_table[-1]
-    last_block              = block_table[last_physical_block_id]
+    last_physical_block_id  = block_table[-1]
+    last_block              = self.blocks[last_physical_block_id]
 
     if len(seq) % self.block_size == 1:
       assert last_block.hash != -1, f"内存块 {last_physical_block_id} 在完整时仍未计算哈希值"
