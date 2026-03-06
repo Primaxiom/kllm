dfbfa40e47979fab4e52ebb0d3f63a5c4acd9e69
Kagamin0707 <1654824865@qq.com>
2026-03-06 22:27:06 +0800

    feat(engine): 实现调度器类的 schedule 函数

--- vllm/engine/scheduler.py
+++ vllm/engine/scheduler.py
@@ -26,7 +26,40 @@ class Scheduler:
     self.waiting_seqs.appendleft(seq)
 
   def schedule(self) -> tuple[list[Sequence], bool]:
-    pass
+    scheduled_seqs      = []
+    num_batched_tokens  = 0
+
+    while self.waiting_seqs \
+      and self.max_num_seqs > len(scheduled_seqs):
+      seq = self.waiting_seqs[0]
+      if not self.block_manager.can_allocate(seq) \
+          or self.max_num_batched_tokens < len(seq) + num_batched_tokens - seq.num_cached_tokens:
+        break
+      self.block_manager.allocate(seq)
+      self.waiting_seqs.popleft()
+      self.running_seqs.append(seq)
+      seq.status = SequenceStatus.RUNNING
+      scheduled_seqs.append(seq)
+      num_batched_tokens += len(seq) - seq.num_cached_tokens
+    if scheduled_seqs:
+      return scheduled_seqs, True
+    
+    while self.running_seqs \
+      and self.max_num_seqs > len(scheduled_seqs):
+      seq = self.running_seqs.popleft()
+      while not self.block_manager.can_append(seq):
+        if self.running_seqs:
+          self.preempt(self.running_seqs.pop())
+        else:
+          self.preempt(seq)
+          break
+      else:
+        scheduled_seqs.append(seq)
+        self.block_manager.may_append(seq)
+    assert scheduled_seqs, "无任何序列可调度"
+    self.running_seqs.appendleft(reversed(scheduled_seqs))
+    
+    return scheduled_seqs, False
 
   def postprocess(self, seqs: list[Sequence], token_ids: list[int]) -> list[bool]:
     pass
