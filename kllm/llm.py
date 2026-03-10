82a5370abe4777f9104f77aa44fd70ba96259c6d
Kagamin0707 <1654824865@qq.com>
2026-03-10 16:42:03 +0800

    feat(llm): 实现 LLM 类基本函数, 包括 tokenize, detokenize, abort, exit

--- kllm/llm.py
+++ kllm/llm.py
@@ -1,6 +1,6 @@
 
 
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, PreTrainedTokenizer
 import asyncio
 
 from kllm.config import Config
@@ -13,18 +13,20 @@ class LLM:
   def __init__(self, cfg: Config):
     self.cfg                            = cfg
     self.client                         = EngineClient(cfg)
-    self.tokenizer                      = AutoTokenizer.from_pretrained(cfg.model)
+    self.tokenizer: PreTrainedTokenizer = AutoTokenizer.from_pretrained(cfg.model, trust_remote_code=True)
     self.request_states: RequestStates  = {}
     self.output_processor_task          = asyncio.create_task(self.output_processor())
 
-  def tokenize(self):
-    pass
+  def tokenize(self, prompts: list[str]) -> list[list[int]]:
+    return self.tokenizer(prompts)["input_ids"]
 
-  def detokenize(self):
-    pass
+  def detokenize(self, token_ids: list[list[int]]) -> list[str]:
+    return self.tokenizer.batch_decode(token_ids, skip_special_tokens=True)
 
-  def abort(self):
-    pass
+  def abort(self, seq: str):
+    if seq in self.request_states:
+      self.client.abort_request(seq)
+      del self.request_states[seq]
 
   async def generate(self):
     pass
@@ -33,4 +35,5 @@ class LLM:
     pass
 
   async def exit(self):
-    pass
+    self.output_processor_task.cancel()
+    await asyncio.to_thread(self.client.exit)
